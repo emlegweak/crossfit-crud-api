1ae550da09fa822d8c4e78f4ed6fe0853e5827b2
emlegweak <emilythearmstrong@gmail.com>
2022-07-20 17:37:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+created a crossfit crud api using javascript, node.js, express.js
+
+documentation powered by swagger - an API description format for REST APIs, where API specs are written in JSON using Swagger Editor, Swagger UI
+
+definitely want to get the database itself on mongodb and the api pushed to heroku
---
+++ src/controllers/recordController.js
@@ -0,0 +1,21 @@
+const recordService=require("../services/recordService");
+
+const getRecordForWorkout=(req,res)=>{
+    const {
+        params: { workoutId },
+    } = req;
+    if (!workoutId) {
+        res.status(400).send({ status: "FAILED", data: { error: "Request failed, please ensure that all inputs are correct. :workoutId cannot be empty." }, });
+        return;
+    }
+    try {
+        const record = recordService.getRecordForWorkout(workoutId);
+        res.send({ status: "OK", data: record });
+    } catch (error) {
+        res.status(error?.status || 500).send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+}
+
+module.exports={
+    getRecordForWorkout,
+}
